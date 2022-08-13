8d7d160701eb3facfcb7be3d08b2698b60f3318b
H7NTER_RXMAN <68168932+mosamir1110ow9w929@users.noreply.github.com>
2022-08-13 16:15:42 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,10 @@
 import random
+# Variables
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" #Letters
+numbers = "0123456789" # Numbers
+everything = letters + numbers ## That Variable Creates The Code
+characters = 16  # Fortnite Gift Card Codes Should Be 16 Characters
 
-upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-all = upper_case + numbers
-length = 16
-
-gene = ''.join(random.sample(all, length))
-print("Code Is", gene)
+# Display Code
+gen = ''.join(random.sample(everything, characters)) # That Will Create The Code With Its Letters And Numbers And Its Characters
+print("Code Is", gen) # It Prints The Code
